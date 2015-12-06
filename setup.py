26a03dc3c50f2e8685060318282c9dd43867ba02
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2015-12-06 16:28:04 +1300

    Preparing for 0.0.1 release

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pccora',
+      version='0.0.1',
+      description='PC-CORA sounding data files parser for Python',
+      url='http://github.com/kinow/pccora',
+      author='Bruno P. Kinoshita',
+      author_email='brunodepaulak@yahoo.com.br',
+      license='MIT',
+      packages=['pccora'],
+      zip_safe=False)

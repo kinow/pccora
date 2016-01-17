6c1bb99cb877200b198904373294c658e080935f
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-01-17 18:28:48 +1300

    Modifying setup.py for 0.1 release

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 from setuptools import setup
 
 setup(name='pccora',
-      version='0.0.1',
+      version='0.1',
       description='PC-CORA sounding data files parser for Python',
-      url='http://github.com/kinow/pccora',
+      url='http://github.com/niwa/pccora',
       author='Bruno P. Kinoshita',
       author_email='brunodepaulak@yahoo.com.br',
       license='MIT',
+      keywords = ['sounding file', 'radiosonde', 'vaisala', 'pccora'],
       packages=['pccora'],
       zip_safe=False)

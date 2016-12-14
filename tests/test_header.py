e57ad042522f70b4641471287254dc90d9d72d4a
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-12-14 17:30:31 +1300

    Fix test

--- tests/test_header.py
+++ tests/test_header.py
@@ -7,5 +7,5 @@ class TestHeader(unittest.TestCase):
 
     def test_header(self):
         h = pccora.pccora.pccora_header
-        struct = h.parse()
-        self.assertEqual('copytest', struct['copyright'])
+        struct = h.parse(b'(C) copytest        ^@^@^@^@<C4>^@<97>^_<D1>^B^L^@^B^@(^@^B^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^A^@^D^@^B^@9^A<99>^C3!^D^@^@^D=^@^@<80>^@^@^@^@<B4>^A')
+        self.assertEqual('(C) copytest', struct['copyright'].strip())

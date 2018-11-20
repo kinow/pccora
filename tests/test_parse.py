8b703dc7c66702051b9f1c16cdd4ea9b87bfc315
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2018-11-20 14:07:32 +1300

    Fix test files location

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from pccora import PCCORAParser
@@ -10,12 +11,14 @@ class TestParse(unittest.TestCase):
 
     def setUp(self):
         self.parser = PCCORAParser()
+        self.s_file_name = os.path.join(os.path.dirname(__file__), "93011809.21S")
+        self.z_file_name = os.path.join(os.path.dirname(__file__), "93011809.21Z")
 
     def test_constructor(self):
         self.assertIsNone(self.parser.result)
 
     def test_parse(self):
-        self.parser.parse_file('93011809.21S')
+        self.parser.parse_file(self.s_file_name)
         self.assertIsNotNone(self.parser.result)
         result = self.parser.get_result()
         self.assertEqual(result, self.parser.result)
@@ -23,23 +26,23 @@ class TestParse(unittest.TestCase):
         self.assertTrue("Vaisala" in header.copyright)
 
     def test_parse_stream(self):
-        with open('93011809.21S', 'rb') as file_stream:
+        with open(self.s_file_name, 'rb') as file_stream:
             self.parser.parse_stream(file_stream)
             header = self.parser.get_header()
             self.assertTrue("Vaisala" in header.copyright)
 
     def test_parse_s_file(self):
-        self.parser.parse_s_file('93011809.21S')
+        self.parser.parse_s_file(self.s_file_name)
         header = self.parser.get_header()
         self.assertTrue("Vaisala" in header.copyright)
 
     def test_parse_z_file(self):
-        self.parser.parse_z_file('93011809.21Z')
+        self.parser.parse_z_file(self.z_file_name)
         header = self.parser.get_header()
         self.assertTrue("Vaisala" in header.copyright)
 
     def test_parse_header(self):
-        self.parser.parse_file('93011809.21S')
+        self.parser.parse_file(self.s_file_name)
         header = self.parser.get_header()
         self.assertTrue("Vaisala" in header.copyright)
         self.assertEqual(196, header.identification_length)
@@ -47,7 +50,7 @@ class TestParse(unittest.TestCase):
         self.assertTrue(header.data_records > 0)
 
     def test_parse_identification(self):
-        self.parser.parse_file('93011809.21S')
+        self.parser.parse_file(self.s_file_name)
         identification = self.parser.get_identification()
         # TBD: funny the block number to be 2?!
         self.assertEqual(2, identification.wmo_block_number)
@@ -65,7 +68,7 @@ class TestParse(unittest.TestCase):
         self.assertTrue(identification.reference_temperature > 23)
 
     def test_parse_data(self):
-        self.parser.parse_file('93011809.21S')
+        self.parser.parse_file(self.s_file_name)
         data = self.parser.get_data()
         # TBD: funny that the length of data here does not match with header
         self.assertTrue(len(data) > 0)
@@ -75,7 +78,7 @@ class TestParse(unittest.TestCase):
             self.assertTrue(entry.pressure >= 0.0)
 
     def test_parse_hires_data(self):
-        self.parser.parse_z_file('93011809.21Z')
+        self.parser.parse_z_file(self.z_file_name)
         hires_data = self.parser.get_hires_data()
         # TBD: funny that the length of hires data here does not match with header
         self.assertTrue(len(hires_data) > 0)
@@ -85,6 +88,6 @@ class TestParse(unittest.TestCase):
             self.assertIsNotNone(entry.temperature)
 
     def test_parse_syspar(self):
-        self.parser.parse_file('93011809.21S')
+        self.parser.parse_file(self.s_file_name)
         syspar = self.parser.get_syspar()
         self.assertTrue(len(syspar) > 0)
